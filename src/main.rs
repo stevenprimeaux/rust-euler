849740dcce05393232de3abd4f0f4cb6931ac2e0
stevenprimeaux <stevenprimeaux@gmail.com>
2023-07-20 17:02:33 -0500

    Add largest prime factor.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use num::integer::sqrt;
+
 fn main() {
     println!("{}", sum_divisible_by_2(1000, 3, 5));
     println!("{}", fibonacci_sum_even_upto(4000000));
+    println!("{}", largest_prime_factor(600851475143));
 }
 
 fn sum_divisible_by(below: u32, mult: u32) -> u32 {
@@ -39,4 +42,53 @@ fn fibonacci_sum_even_upto(upto: u32) -> u32 {
     }
 
     current_sum
+}
+
+fn largest_prime_factor(dividend: u64) -> u64 {
+    let mut current_dividend: u64 = dividend;
+
+    if current_dividend == 1 {
+        return 1;
+    }
+
+    if current_dividend % 2 == 0 {
+        current_dividend /= 2;
+        while current_dividend % 2 == 0 {
+            current_dividend /= 2;
+        }
+        if current_dividend == 1 {
+            return 2;
+        }
+    }
+
+    let mut current_sqrt: u64 = sqrt(current_dividend);
+    let mut current_largest: u64 = current_dividend;
+    loop {
+        let mut is_prime: bool = true;
+        let mut current_try: u64 = 3;
+        while current_dividend > 1 && current_try <= current_sqrt {
+            if current_dividend % current_try == 0 {
+                current_largest = current_try;
+                while current_dividend % current_try == 0 {
+                    current_dividend /= current_try
+                }
+
+                current_sqrt = sqrt(current_dividend);
+                is_prime = false;
+                break;
+            }
+
+            current_try += 2
+        }
+
+        if is_prime {
+            break;
+        }
+    }
+
+    if current_dividend == 1 {
+        return current_largest;
+    }
+
+    return current_dividend;
 }
