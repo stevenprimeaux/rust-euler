7eb460ec287e0cb38ffefaa201c3a96a3a4eb4a1
stevenprimeaux <stevenprimeaux@gmail.com>
2023-08-01 14:35:51 -0500

    Reformat Pythagorean triple.

--- src/main.rs
+++ src/main.rs
@@ -226,21 +226,24 @@ fn gcd(a: u64, b: u64) -> u64 {
 }
 
 fn pythagorean_triple(s: u64) -> u64 {
-    let s_half: u64 = s / 2;
-    let m_limit: u64 = ((s as f64).sqrt().ceil() - 1.0) as u64;
+    let s_2: u64 = s / 2;
+    let mut s_m: u64;
 
     let mut k: u64;
-    let mut s_div_m: u64;
+    let m_limit: u64 = ((s as f64).sqrt().ceil() - 1.0) as u64;
+
+    let d: u64;
+    let n: u64;
 
     let mut a: u64 = 0;
     let mut b: u64 = 0;
     let mut c: u64 = 0;
 
     'outer: for m in 2..m_limit {
-        if s_half % m == 0 {
-            s_div_m = s_half / m;
-            while s_div_m % 2 == 0 {
-                s_div_m /= 2;
+        if s_2 % m == 0 {
+            s_m = s_2 / m;
+            while s_m % 2 == 0 {
+                s_m /= 2;
             }
 
             if m % 2 == 1 {
@@ -249,13 +252,13 @@ fn pythagorean_triple(s: u64) -> u64 {
                 k = m + 1;
             }
 
-            while (k < 2 * m) & (k <= s_div_m) {
-                if (s_div_m % k == 0) & (gcd(k, m) == 1) {
-                    let d = s_half / (k * m);
-                    let n = k - m;
+            while (k < 2 * m) & (k <= s_m) {
+                if (s_m % k == 0) & (gcd(k, m) == 1) {
+                    d = s_2 / (k * m);
+                    n = k - m;
 
                     a = d * (m * m - n * n);
-                    b = 2 * d * m * n;
+                    b = d * (2 * m * n);
                     c = d * (m * m + n * n);
 
                     break 'outer;
