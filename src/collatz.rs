91f23bec98ca9401a8c1f8e74292d20826925270
stevenprimeaux <stevenprimeaux@gmail.com>
2023-08-12 21:08:22 -0500

    Optimize Collatz sequence, although may be slower.

--- src/collatz.rs
+++ src/collatz.rs
@@ -1,26 +1,34 @@
-pub fn collatz(n: u64, count: usize, print: bool) -> (u64, usize) {
-    if n == 1 {
-        return (1, count);
-    } else if n % 2 == 0 {
-        return collatz(n / 2, count + 1, print);
-    } else {
-        return collatz((3 * n) + 1, count + 1, print);
+use std::collections::HashMap;
+
+pub fn collatz(n: u64, counts: &mut HashMap<u64, usize>) -> usize {
+    if counts.get(&n).is_some() {
+        return counts[&n];
     }
+
+    let count: usize;
+    if n % 2 == 0 {
+        count = 1 + collatz(n / 2, counts);
+    } else {
+        count = 2 + collatz((3 * n + 1) / 2, counts);
+    }
+    counts.insert(n, count);
+
+    counts[&n]
 }
 
 pub fn collatz_try(n: u64) -> u64 {
-    let mut num_best = 0;
-    let mut len_max = 0;
+    let mut best: u64 = 0;
+    let mut len_max: usize = 0;
+    let mut counts: HashMap<u64, usize> = HashMap::from([(1, 1)]);
 
-    let mut len_current;
-    for i in (1..=n).rev() {
-        len_current = collatz(i, 1, false).1;
+    let mut len_current: usize;
+    for i in (n / 2)..=n {
+        len_current = collatz(i, &mut counts);
         if len_current > len_max {
             len_max = len_current;
-            num_best = i;
+            best = i;
         }
     }
-    println!("{} {}", num_best, len_max);
 
-    num_best
+    best
 }
