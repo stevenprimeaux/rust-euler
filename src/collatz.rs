20d67bf284a894cb8fd4794999d627e540994c18
stevenprimeaux <stevenprimeaux@gmail.com>
2023-08-14 14:43:58 -0500

    Add back brute force Collatz.

--- src/collatz.rs
+++ src/collatz.rs
@@ -1,21 +1,5 @@
 use std::collections::HashMap;
 
-pub fn collatz(n: u64, counts: &mut HashMap<u64, usize>) -> usize {
-    if counts.get(&n).is_some() {
-        return counts[&n];
-    }
-
-    let count: usize;
-    if n % 2 == 0 {
-        count = 1 + collatz(n / 2, counts);
-    } else {
-        count = 2 + collatz((3 * n + 1) / 2, counts);
-    }
-    counts.insert(n, count);
-
-    counts[&n]
-}
-
 pub fn collatz_try(n: u64) -> u64 {
     let mut best: u64 = 0;
     let mut len_max: usize = 0;
@@ -23,7 +7,7 @@ pub fn collatz_try(n: u64) -> u64 {
 
     let mut len_current: usize;
     for i in (n / 2)..=n {
-        len_current = collatz(i, &mut counts);
+        len_current = collatz_map(i, &mut counts);
         if len_current > len_max {
             len_max = len_current;
             best = i;
@@ -31,4 +15,31 @@ pub fn collatz_try(n: u64) -> u64 {
     }
 
     best
+}
+
+#[allow(dead_code)]
+pub fn collatz_brute(n: u64, count: usize) -> (u64, usize) {
+    if n == 1 {
+        return (1, count);
+    } else if n % 2 == 0 {
+        return collatz_brute(n / 2, count + 1);
+    } else {
+        return collatz_brute(3 * n + 1, count + 1);
+    }
+}
+
+pub fn collatz_map(n: u64, counts: &mut HashMap<u64, usize>) -> usize {
+    if counts.get(&n).is_some() {
+        return counts[&n];
+    }
+
+    let count: usize;
+    if n % 2 == 0 {
+        count = 1 + collatz_map(n / 2, counts);
+    } else {
+        count = 2 + collatz_map((3 * n + 1) / 2, counts);
+    }
+    counts.insert(n, count);
+
+    counts[&n]
 }
