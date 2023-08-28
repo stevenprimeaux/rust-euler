075b70875c6a5706ae06abe2c60b402d86755079
stevenprimeaux <stevenprimeaux@gmail.com>
2023-08-28 18:42:09 -0500

    Simplify variable names.

--- src/euler/fibonacci.rs
+++ src/euler/fibonacci.rs
@@ -10,17 +10,17 @@ pub fn fib_n_even(n: usize) -> u32 {
 }
 
 pub fn fib_sum_even(limit: u32) -> u32 {
-    let mut current_index: usize = 1;
-    let mut current_term: u32 = 2;
-    let mut current_sum: u32 = 0;
+    let mut index: usize = 1;
+    let mut term: u32 = 2;
+    let mut sum: u32 = 0;
 
-    while current_term <= limit {
-        current_sum += current_term;
-        current_index += 1;
-        current_term = fib_n_even(current_index);
+    while term <= limit {
+        sum += term;
+        index += 1;
+        term = fib_n_even(index);
     }
 
-    current_sum
+    sum
 }
 
 #[cfg(test)]
