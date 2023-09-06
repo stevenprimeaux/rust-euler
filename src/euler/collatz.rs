d7ee8ea58e9a32978a81a1fc4b61bb13eec9e067
stevenprimeaux <stevenprimeaux@gmail.com>
2023-09-06 10:19:29 -0500

    Add tests for Collatz sequence.

--- src/euler/collatz.rs
+++ src/euler/collatz.rs
@@ -1,23 +1,5 @@
 use std::collections::HashMap;
 
-pub fn collatz_try(n: u64) -> u64 {
-    let mut best: u64 = 0;
-    let mut len_max: usize = 0;
-    let mut counts: HashMap<u64, usize> = HashMap::from([(1, 1)]);
-
-    let mut len_current: usize;
-    for i in (n / 2)..=n {
-        len_current = collatz_map(i, &mut counts);
-        if len_current > len_max {
-            len_max = len_current;
-            best = i;
-        }
-    }
-
-    best
-}
-
-#[allow(dead_code)]
 pub fn collatz_brute(n: u64, count: usize) -> (u64, usize) {
     if n == 1 {
         return (1, count);
@@ -42,4 +24,42 @@ pub fn collatz_map(n: u64, counts: &mut HashMap<u64, usize>) -> usize {
     counts.insert(n, count);
 
     counts[&n]
+}
+
+pub fn collatz_try(n: u64) -> u64 {
+    let mut best: u64 = 0;
+    let mut len_max: usize = 0;
+    let mut counts: HashMap<u64, usize> = HashMap::from([(1, 1)]);
+
+    let mut len_current: usize;
+    for i in (n / 2)..=n {
+        len_current = collatz_map(i, &mut counts);
+        if len_current > len_max {
+            len_max = len_current;
+            best = i;
+        }
+    }
+
+    best
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_collatz_brute() {
+        assert_eq!(collatz_brute(13, 1).1, 10);
+    }
+
+    #[test]
+    fn test_collatz_map() {
+        let mut counts: HashMap<u64, usize> = HashMap::from([(1, 1)]);
+        assert_eq!(collatz_map(13, &mut counts), 10);
+    }
+
+    #[test]
+    fn test_collatz_try() {
+        assert_eq!(collatz_try(999999), 837799);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ fn main() {
     println!("08: {}", grid::grid_prod_max_rowwise(data_08(), 13));
     println!("11: {}", grid::grid_prod_max_alldir(data_11(), 20, 20, 4));
     println!("13: {}", grid::grid_sum_rows(data_13()));
-    println!("14: {}", collatz::collatz_try(999999));
     println!("15: {}", paths::path_count(20));
     println!("16: {}", overflow::oflow_pow(2, 1000));
     println!("17: {}", letters::letters_count_chars_cum(1000));
