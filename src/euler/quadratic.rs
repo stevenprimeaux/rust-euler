80f117a8d9304a658ab1feec8864d22d8883bf55
stevenprimeaux <stevenprimeaux@gmail.com>
2024-02-21 19:10:53 -0600

    Provide all coefficients as arguments to quad_eval.

--- src/euler/quadratic.rs
+++ src/euler/quadratic.rs
@@ -1,25 +1,47 @@
 use crate::primes;
 
-pub fn quad_primes_count(a: i64, b: i64) -> i64 {
+/// Returns value of quadratic function
+/// with coefficients a, b, c
+/// and independent variable x.
+pub fn quad_eval(a: i64, b: i64, c: i64, x: i64) -> i64 {
+    (a * x.pow(2)) + (b * x) + c
+}
+
+/// Given an upper bound on coefficients b and c of a quadratic function,
+/// return the product of b and c for the function that generates
+/// the most consecutive prime numbers.
+///
+/// # Examples
+///
+/// ```
+/// use rust_euler::quadratic::quad_primes_count;
+///
+/// // x^2 + x + 41, 40 primes from x = 0 to x = 39
+/// assert_eq!(quad_primes_count(1, 1, 41), -41);
+///
+/// // x^2 - 79x + 1601, 80 primes from x = 0 to x = 79
+/// assert_eq!(quad_primes_count(1, 79, 1601), -126479);
+/// ```
+pub fn quad_primes_count(a: i64, b: i64, c: i64) -> i64 {
     let mut n_max: i64 = 0;
     let mut prod_best: i64 = 0;
 
     let mut n_current: i64;
-    for a in (-1 * a)..=a {
-        'b: for b in (-1 * b)..=b {
+    for b in (-b)..=b {
+        'c: for c in (-c)..=c {
             n_current = 0;
             loop {
-                match primes::is_prime(quad_eval(a, b, n_current).abs().try_into().unwrap()) {
+                match primes::is_prime(quad_eval(a, b, c, n_current).abs().try_into().unwrap()) {
                     true => n_current += 1,
                     false => {
                         match n_current > n_max {
                             true => {
                                 n_max = n_current;
-                                prod_best = a * b;
+                                prod_best = b * c;
                             }
                             false => (),
                         }
-                        continue 'b;
+                        continue 'c;
                     }
                 }
             }
@@ -29,18 +51,19 @@ pub fn quad_primes_count(a: i64, b: i64) -> i64 {
     prod_best
 }
 
-pub fn quad_eval(a: i64, b: i64, n: i64) -> i64 {
-    n.pow(2) + (a * n) + b
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
+    fn test_quad_eval() {
+        assert_eq!(quad_eval(0, 0, 0, 0), 0);
+        assert_eq!(quad_eval(1, 1, 1, 1), 3);
+        assert_eq!(quad_eval(2, 2, 2, 2), 14);
+    }
+
+    #[test]
     fn test_quad_primes_count() {
-        assert_eq!(quad_primes_count(1, 41), -41);
-        assert_eq!(quad_primes_count(79, 1601), -126479);
-        assert_eq!(quad_primes_count(1000, 1000), -59231);
+        assert_eq!(quad_primes_count(1, 1000, 1000), -59231);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use rust_euler::*;
 
 #[cfg(not(tarpaulin_include))]
 fn main() {
-    println!("\n27: {}", quadratic::quad_primes_count(1000, 1000));
+    println!("\n27: {}", quadratic::quad_primes_count(1, 1000, 1000));
 }
